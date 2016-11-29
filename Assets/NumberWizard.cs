a75aa7d5d008d5069c369b861d15eb54d18cce5d
Liane Stevenson <knightowlcrm@gmail.com>
2016-11-29 22:32:48 +0000

    Game Files Added
    From Unity 4.6

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Var Initialization
+	int max;
+	int min;
+	int guess;
+	
+	//Settings
+	public int maxGuessesAllowed = 5;
+
+	//UI
+	public Text guessText;
+	
+	// Game Start
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame() { 
+		max = 1000;
+		min = 1;
+		guess = Random.Range(min, max+1);
+		guessText.text = guess.ToString();
+	}
+	
+	public void GuessHigher () { 
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower () { 
+		max = guess;
+		NextGuess();
+	}
+	
+	void NextGuess() { 
+		guess = Random.Range(min, max+1);
+		guessText.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) { 
+			Application.LoadLevel("Win");
+		}
+	}
+}
